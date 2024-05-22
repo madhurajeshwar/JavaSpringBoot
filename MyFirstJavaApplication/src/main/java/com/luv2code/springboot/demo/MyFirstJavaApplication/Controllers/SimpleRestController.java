8f0899b066d22c6caf06f193ac0204c7ee75742b
Rajeshwar Madhu <rajeshwarmadhu@Rajeshwars-MBP.lan>
2024-05-22 17:30:42 -0500

    Added new dependency in pom.xml in order to restart the server automatically once we make any changes in controller (Eliminating the need for manual configuration)

--- MyFirstJavaApplication/src/main/java/com/luv2code/springboot/demo/MyFirstJavaApplication/Controllers/SimpleRestController.java
+++ MyFirstJavaApplication/src/main/java/com/luv2code/springboot/demo/MyFirstJavaApplication/Controllers/SimpleRestController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleRestController {
+    /*This is a Simple Rest Controller that has 2 mappings*/
     @GetMapping("/")
-    /**/
     public String greet(){
         return "Hey Lets rock It";
     }
+
+// This is for testing spring-boot-dev-tools added in pom.xml
+    @GetMapping("/workOut")
+    public String workOut(){
+        return "you need to exercise";
+    }
+
 }
